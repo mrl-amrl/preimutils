b350e31f98018325aa7e345f2ea95762b4b83217
amir <ami_rsh@outlook.com>
2020-02-19 00:49:59 +0330

    setup changed

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
 import os
 
-if os.environ.get('CI_COMMIT_TAG'):
-    version = os.environ['CI_COMMIT_TAG']
-else:
-    version = os.environ['CI_JOB_ID']
 
 setup(
     name="preimutils",
     packages=['preimutils'],
-    version=version,
+    version='1.0.4',
     description="all you need to prepare and preprocess your annotated images",
     url="https://github.com/mrl-amrl/preimutils",
-    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.0.tar.gz',
+    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.4.tar.gz',
     author="Amir Sharifi",
     author_email="ami_rsh@outlook.com",
     license='MIT',
