9f2931a6211731b780394dccbe1593d52cb1a9ca
amir <ami_rsh@outlook.com>
2020-04-23 16:41:50 +0430

    setup.py added

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+import os
+
+
+setup(
+    name="preimutils",
+    packages=find_packages(),
+    version='1.0.4',
+    description="all you need to prepare and preprocess your annotated images",
+    url="https://github.com/mrl-amrl/preimutils",
+    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.4.tar.gz',
+    author="Amir Sharifi",
+    author_email="ami_rsh@outlook.com",
+    license='MIT',
+    keywords=['computer vision', 'image processing', 'opencv',
+              'matplotlib', 'preprocess image', 'image dataset', 'pascal voc'],
+    entry_points={
+        'console_scripts': ['preimutils=preimutils.run:main'],
+    },
+    install_requires=[
+        'pascal_voc_writer',
+        'tqdm',
+        'pandas',
+        'albumentations',
+    ],
+    zip_safe=False,
+)
