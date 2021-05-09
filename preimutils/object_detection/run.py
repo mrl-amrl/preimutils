606e6676b23643eea90d64b7f4a265c8e1c4c17c
amir sharifi <amir_sh_50@outlook.com>
2021-05-09 10:15:45 +0430

    setup and run file updated

--- preimutils/object_detection/run.py
+++ preimutils/object_detection/run.py
@@ -1,8 +1,11 @@
 import argparse
 import os
-from .label_json import LabelHandler, AMRLImageAug, label_checker, replace_label, cut_with_object_names, xml_address_changer
-from preimutils.object_detection import separate_with_label, gather_together, shuffle_img_xml,separate_test_val,separate_test_val,xml_csv_save
-
+from .label_json import LabelHandler
+from preimutils.object_detection import separate_with_label, gather_together, shuffle_img_xml, separate_test_val, separate_test_val, xml_csv_save
+from .check_valid_label import label_checker, replace_label
+from .img_aug import AMRLImageAug
+from .rename_xml_path import xml_address_changer
+from .crop_from_point import cut_with_object_names
 
 
 def main():
@@ -14,13 +17,13 @@ def main():
     ap.add_argument("-j", "--label_json_path", required=True,
                     help="path to label.json file")
     # For auto augment needed
-    ap.add_argument("-q", "--quantity", required=False,type=int,
+    ap.add_argument("-q", "--quantity", required=False, type=int,
                     help="the amount that you want to each object to auto augment")
-    ap.add_argument("-r", "--resize", required=False,default=False,type=bool,
+    ap.add_argument("-r", "--resize", required=False, default=False, type=bool,
                     help="If you want to resize your images set it true")
-    ap.add_argument("-w", "--width", required=False,default=0 ,type=int,
+    ap.add_argument("-w", "--width", required=False, default=0, type=int,
                     help="If you set resize True set the width")
-    ap.add_argument("-he", "--height", required=False,default=0,type=int,
+    ap.add_argument("-he", "--height", required=False, default=0, type=int,
                     help="If you set resize True set the height")
     # For replace_label only
     ap.add_argument("-l", "--label", required=False,
@@ -35,26 +38,26 @@ def main():
     ap.add_argument("-p", "--dataset_dir", required=False,
                     help="path to your dataset file")
     # For train_validation_sep
-    ap.add_argument("-vp", "--validation_persent",type=float,required=False,help="percentage for validation from all of your dataset")
-    
+    ap.add_argument("-vp", "--validation_persent", type=float, required=False,
+                    help="percentage for validation from all of your dataset")
 
     ap.add_argument("-f", "--function", required=True,
                     choices=["auto_augmentation", "label_checker", "replace_label",
                              "cut_with_object_names", "separate_with_label",
                              "gather_together", "shuffle_img_xml",
-                             "separate_test_val","xml_to_csv",'xml_address_changer'] ,help="function that you want to run")
+                             "separate_test_val", "xml_to_csv", 'xml_address_changer'], help="function that you want to run")
 
     args = vars(ap.parse_args())
     # Auto augment
     quantity = args["quantity"]
-    resize= args["resize"]
+    resize = args["resize"]
     width = args["width"]
     height = args["height"]
     # Other func
     images_dir = args['images_dir']
     xmls_dir = args["xmls_dir"]
     json_path = args['label_json_path']
-    
+
     function = args["function"]
     dst_save = args["dst_save"]
     dataset_dir = args["dataset_dir"]
@@ -71,18 +74,19 @@ def main():
         assert os.path.exists(images_dir), 'images path not exist'
         assert quantity > 0, 'Quantity < 0 not acceptable'
         img_aug = AMRLImageAug(json_path, xmls_dir, images_dir)
-        img_aug.auto_augmentation(quantity,resize=resize,width=width,height=height)
+        img_aug.auto_augmentation(
+            quantity, resize=resize, width=width, height=height)
 
     if function == "label_checker":
         assert os.path.exists(xmls_dir), 'xmls path not exist'
         label_array = label_handler.json_label_array()
-        print("label array is ",label_array)
+        print("label array is ", label_array)
         label_checker(xmls_dir, label_array)
 
     if function == "replace_label":
         assert os.path.exists(xmls_dir), 'xmls path not exist'
-        assert not destination_label == None , 'destinaton label param not exist'
-        assert not source_label == None , 'source label param not exist'
+        assert not destination_label == None, 'destinaton label param not exist'
+        assert not source_label == None, 'source label param not exist'
         replace_label(xmls_dir, source_label, destination_label)
 
     if function == "cut_with_object_names":
@@ -115,20 +119,20 @@ def main():
         labels_array = label_handler.json_label_array()
         shuffle_img_xml(xmls_dir, images_dir, dst_save)
 
-
     if function == "separate_test_val":
         assert os.path.exists(xmls_dir), 'xmls path not exist'
         assert os.path.exists(images_dir), 'images path not exist'
         assert persentage > 0, 'persentage LE than 0 not acceptable'
-        train_path = os.path.join(dataset_dir,'train')
-        validation_path = os.path.join(dataset_dir,'validation')
-        separate_test_val(images_dir,xmls_dir,validation_path,train_path,persentage)
-        
+        train_path = os.path.join(dataset_dir, 'train')
+        validation_path = os.path.join(dataset_dir, 'validation')
+        separate_test_val(images_dir, xmls_dir,
+                          validation_path, train_path, persentage)
 
     if function == "xml_to_csv":
         assert os.path.exists(xmls_dir), 'xmls path not exist'
         assert os.path.exists(dst_save), 'dst path not exist'
-        xml_csv_save(xmls_dir,os.path.join(dst_save,'dataframe.csv'))
+        xml_csv_save(xmls_dir, os.path.join(dst_save, 'dataframe.csv'))
+
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,27 +1,38 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 import os
 
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="preimutils",
     packages=find_packages(),
-    version='1.0.5',
-    description="all you need to prepare and preprocess your annotated images",
+    version='1.0.6',
+    description="All you need to prepare and preprocess your annotated images",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/mrl-amrl/preimutils",
-    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.5.tar.gz',
+    download_url='https://github.com/mrl-amrl/preimutils/archive/1.0.6.tar.gz',
     author="Amir Sharifi",
     author_email="ami_rsh@outlook.com",
     license='MIT',
     keywords=['computer vision', 'image processing', 'opencv',
               'matplotlib', 'preprocess image', 'image dataset', 'pascal voc'],
     entry_points={
-        'console_scripts': ['preimutils=preimutils.run:main'],
+        'console_scripts': ['preimutils=preimutils.object_detection.run:main'],
     },
     install_requires=[
         'pascal_voc_writer',
         'tqdm',
-        'pandas',
         'albumentations',
+        'pandas',
+        'imutils',
+        'opencv-python',
+        'imageio',
+        'pycocotools',
+        'shutils'
     ],
     zip_safe=False,
 )
