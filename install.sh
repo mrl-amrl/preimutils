d41f2e08ff1fb6d9e1224aba347b28bd07ecf75c
ArianAmani <arianamani.sec@gmail.com>
2022-01-09 20:50:02 +0330

    Segmentations: utils, dataset and augment completed and debugged

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 rm -rf build dist preimutils.egg-info
-python setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 pip install --upgrade .
--- preimutils/segmentations/voc/augment.py
+++ preimutils/segmentations/voc/augment.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
-from . import utils 
+from . import utils
 # from .utils import export_path_count_for_each_label
 from .dataset import LabelMap
 
@@ -37,18 +37,19 @@ class SegmentationAug:
 
     _file_counter = 0
 
-    def __init__(self, label_map_path, masks_dir, images_dir):
-  
+    def __init__(self, label_map_path, masks_dir, images_dir, images_extention='jpg'):
 
-        assert os.path.exists(label_map_path), 'label_map .txt file not exist'
+        self.images_extention = images_extention
+
+        assert os.path.exists(label_map_path), 'label_map.txt file not exist'
         self.label_handler = LabelMap(label_map_path)
 
         assert os.path.exists(
-            masks_dir), 'XML path not exist please check the path'
+            masks_dir), 'Masks path does not exist please check the path'
         self._masks_dir = masks_dir
 
         assert os.path.exists(
-            images_dir), 'Image path not exist please check the image path'
+            images_dir), 'Images path does not exist please check the image path'
         self._images_dir = images_dir
         self.filters_of_aug = [
 
@@ -69,9 +70,10 @@ class SegmentationAug:
             A.MedianBlur(p=0.2),
             A.ISONoise(p=0.2),
             A.Posterize(p=0.2),
-            A.IAAPerspective(p=0.1),
-            A.IAAPiecewiseAffine(p=0.1, scale=(0.01, 0.02)),
-            A.IAAEmboss(p=0.2),
+            A.augmentations.geometric.transforms.Perspective(p=0.1),
+            A.augmentations.geometric.transforms.PiecewiseAffine(
+                p=0.1, scale=(0.01, 0.02)),
+            A.augmentations.transforms.Emboss(p=0.2),
         ]
 
     def _get_aug(self, aug):
@@ -94,49 +96,54 @@ class SegmentationAug:
 
             No return
         """
+        aug = self.filters_of_aug.copy()
 
-        # if resize:
-        #     filters_of_aug.append(A.Resize(width, height, always_apply=True))
-        aug = self._get_aug(self.filters_of_aug)
-        image_path = utils.find_image_from_mask(mask_path, self._images_dir)
-        image = cv2.imread(image_path)
-        mask = cv2.imread(mask_path)
+        if resize:
+            aug.append(A.Resize(width, height, always_apply=True))
+        aug = self._get_aug(aug)
+        image_path = utils.find_image_from_mask(
+            mask_path, self._images_dir, extention=self.images_extention)
+        image = cv2.imread(str(image_path[0]))
+        mask = cv2.imread(str(mask_path))
         for i in tqdm(range(quantity), desc='Singel image'):
-            augmented = aug(image=image, mask=mask)
+            augmented = aug(image=np.array(image), mask=np.array(mask))
             nimage = augmented['image']
             nmask = augmented['mask']
             SegmentationAug._file_counter += 1
             file_name = 'aug_{}'.format(SegmentationAug._file_counter)
-            nimage_path = os.path.join(self._images_dir, file_name + '.jpg')
+            nimage_path = os.path.join(
+                self._images_dir, file_name + '.' + self.images_extention)
             nmask_path = os.path.join(self._masks_dir, file_name + '.png')
             cv2.imwrite(nimage_path, nimage)
             cv2.imwrite(nmask_path, nmask)
 
-    def auto_augmentation(self, count_of_each):
+    def auto_augmentation(self, count_of_each, resize=False, width=0, height=0):
         """auto augmentation for each picture depend on statistic of the object exist in your dataset
         if your image and mask names are same
         save your aug image in your dataset path with the following pattern aug_{counter}.jpg
 
         Args:
 
-            count_of_each(int): How much of each label you want to have !
+            count_of_each(int): How much of each label you want to have(counting the images you already have)
+            resize:(bool : optional)-> defult False ... resize your augmented images
+            width:(int : optional) width for resized ... if resize True you should use this arg
+            height:(int : optional) height for resized... if resize True you should use this arg
 
         Returns:
-        
+
             No return
         """
         labels_statistics = utils.export_path_count_for_each_label(
-            self.label_handler.color_label(), self._images_dir, self._masks_dir)
+            self.label_handler.color_label(), self._images_dir, self._masks_dir, extention=self.images_extention)
         for label in tqdm(self.label_handler.label_color()):
-            print(label)
             count = labels_statistics[label]['count']
             masks_paths = labels_statistics[label]['masks_path']
             if not count:
                 continue
             coefficient = count_of_each // count
-            print(coefficient)
             for mask in masks_paths:
-                self.augment_image(mask, coefficient)
+                self.augment_image(mask, coefficient,
+                                   resize=resize, width=width, height=height)
 
     def encode_mask_dataset(self, class_color):
         """encode color map dataset masks to 1 channel mask used for most semantic segmentation models
@@ -147,14 +154,14 @@ class SegmentationAug:
             class_color(int):   [(r,g,b),(r,g,b),(r,g,b),...]
 
         Returns:
-        
+
             No return
         """
         dst_path = os.path.join(self._masks_dir, 'pre_encoded')
         if not os.path.exists(dst_path):
             os.mkdir(dst_path)
-        
-        for mask_path in tqdm(glob(os.path.join(self._masks_dir, '*.png'))[:1]):
+
+        for mask_path in tqdm(glob(os.path.join(self._masks_dir, '*.png'))):
             mask_basename = os.path.basename(mask_path)
             mask = Image.open(mask_path)
             mask = np.asarray(mask)
