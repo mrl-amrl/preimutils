d934144c99d78bc12c0e66faabfbc0ac1722e800
a-sharifi <ami_rsh@outlook.com>
2022-01-27 12:35:48 +0330

    Update setup.py
    version updated keypoint detection added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-version='1.1.1'
+version='1.1.2'
 setup(
     name="preimutils",
     packages=find_packages(),
